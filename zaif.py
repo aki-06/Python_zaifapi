62f8607dbff8a264e4dbf02f6414d50b8a14a184
akihironemoto <akihiro.nemoto.06@gmail.com>
2017-10-30 23:10:47 +0900

    fix

--- zaif.py
+++ zaif.py
@@ -1,12 +1,6 @@
-from zaifapi import *
+from zaifapi.impl import ZaifPublicApi
 
+#ビットコインと日本円の終値を取得
+zaif = ZaifPublicApi()
+print(zaif.last_price('btc_jpy'))
 
-btc_jpy = zaif.last_place('btc_jpy')
-xem_jpy = zaif.last_place('xem_jpy')
-btc_price_jpy = zaif.last_place('btc_jpy')['last_place']
-xem_price_jpy = zaif.last_place('btc_jpy')['last_place']
-
-print(btc_jpy)
-print(xem_jpy)
-print(btc_price_jpy)
-print(xem_price_jpy)
